b2699bbc43ba0698963b0ef1091dcaf3835fc601
David Hoe <david_hoe@hotmail.com>
2010-06-01 18:18:21 +0100

    updated flocking in include range

--- src/test/physics/FlockingParticlesApp.cpp
+++ src/test/physics/FlockingParticlesApp.cpp
@@ -78,7 +78,7 @@ void FlockingParticlesApp::setup() {
 	emitter->setPosition(space->getCenter());
 	emitter->setInterval(0.01);
 	emitter->setRate(1000);
-	emitter->setMax(50 * 1000);
+	emitter->setMax(1 * 1000);
 	
 	emitter->addBehaviour(new RandomEmitter(space));
 	physics->addBehaviour(new Gravity());
@@ -95,13 +95,17 @@ void FlockingParticlesApp::setup() {
 	physics->addBehaviour(attractor);
 	
 	// flocking
-	FlockAlign* align = new FlockAlign();
+	
+	FlockAlign* align = new FlockAlign(space);
+	align->setRange(0.25);
 	physics->addBehaviour(align);
 	
-	FlockAttract* attract = new FlockAttract();
+	FlockAttract* attract = new FlockAttract(space);
+	attract->setRange(0.25);
 	physics->addBehaviour(attract);
 	
-	FlockRepel* repel = new FlockRepel();
+	FlockRepel* repel = new FlockRepel(space);
+	repel->setRange(0.25);
 	physics->addBehaviour(repel);
 	
 	// init graphics
