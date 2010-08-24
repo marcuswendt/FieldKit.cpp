f8622cb2cc0dde9926bb23c73941c5d7bb429a71
Marcus Wendt <marcus@field.io>
2010-08-24 18:05:36 +0100

    math: fixed ray x line intersection test

--- src/fieldkit/math/Ray.cpp
+++ src/fieldkit/math/Ray.cpp
@@ -60,70 +60,51 @@ bool Ray2f::findIntersection(Ray2f const& ray, Vec2f* result)
 	return true;
 }
 
-
 //! Calculates the intersection between a ray and a line, also checks for parallel lines.
+//! based on Paul Bourke's "Intersection Point Of Two Lines (2 Dimensions)"
+//! http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
 bool Ray2f::findIntersection(Line2f const& line, Vec2f* result)
 {	
 	Vec2f p1 = origin;
 	Vec2f p2 = origin + direction;
-	
-	Vec2f p3 = line.p1;
-	Vec2f p4 = line.p2;
-	
-	float xD1, yD1, xD2, yD2, xD3, yD3;
-	float dot, deg, len1, len2;
-	float segmentLen;
-	float ua, div;
-	
-	// calculate differences
-	xD1 = p2.x - p1.x;
-	xD2 = p4.x - p3.x;
-	yD1 = p2.y - p1.y;
-	yD2 = p4.y - p3.y;
-	xD3 = p1.x - p3.x;
-	yD3 = p1.y - p3.y;
-	
-	// calculate the lenhg gths of the two lines
-	len1 = sqrt(xD1 * xD1 + yD1 * yD1);
-	len2 = sqrt(xD2 * xD2 + yD2 * yD2);
-	
-	// calculate angle between the two lines.
-	dot = (xD1 * xD2 + yD1 * yD2); // dot product
-	deg = dot / (len1 * len2);
-	
-	// if abs(angle) = = 1 then the lines are parallel,
-	// so no intersection is possible
-	if(abs(deg) == 1)
+
+	float denom = ((line.p2.y - line.p1.y) * (p2.x - p1.x)) -
+			 	  ((line.p2.x - line.p1.x) * (p2.y - p1.y));
+
+	float numea = ((line.p2.x - line.p1.x) * (p1.y - line.p1.y)) -
+				  ((line.p2.y - line.p1.y) * (p1.x - line.p1.x));
+
+	float numeb = ((p2.x - p1.x) * (p1.y - line.p1.y)) -
+				  ((p2.y - p1.y) * (p1.x - line.p1.x));
+
+	if(denom == 0.0f) {
+		if(numea == 0.0f && numeb == 0.0f) {
+			// coincident
+			return false;
+		}
+		// parallel
 		return false;
-	
-	// find intersection Pt between two lines
-	div = yD2 * xD1 - xD2 * yD1;
-	ua = (xD2 * yD3 - yD2 * xD3) / div;
-	
-	Vec2f pt = Vec2f(p1.x + ua * xD1, p1.y + ua * yD1);
-	
-	// calculate the combined length of the two segments
-	// between Pt - p3 and Pt - p4
-	xD1 = pt.x - p3.x;
-	xD2 = pt.x - p4.x;
-	yD1 = pt.y - p3.y;
-	yD2 = pt.y - p4.y;
-	segmentLen = sqrt(xD1 * xD1 + yD1 * yD1)
-			   + sqrt(xD2 * xD2 + yD2 * yD2);
-	
-	// if the lengths of both sets of segments are the same as
-	// the lenghts of the two lines the point is actually
-	// on the line segment.
-	// if the point isn't on the line , return false
-	if(abs(len2 - segmentLen) > 0.01f)
-		return false;
-	
-	// return valid intersection if requested
-	if(result != NULL) {
-		result->x = pt.x;
-		result->y = pt.y;
 	}
 
-	return true;
+	float ua = numea / denom;
+	float ub = numeb / denom;
+
+	// LINE x LINE intersections
+	/*if(result != NULL && 
+	   ua >= 0.0f && ua <= 1.0f && ub >= 0.0f && ub <= 1.0f)*/
+
+	if(ua >= 0.0f && ub > 0.0f && ub < 1.0f) {
+		if(result != NULL) {
+			// Get the intersection point.
+			result->x = p1.x + ua*(p2.x - p1.x);
+			result->y = p1.y + ua*(p2.y - p1.y);
+		}
+
+		// intersecting
+		return true;
+	}
+
+	// not intersecting
+	return false;
 }
 
